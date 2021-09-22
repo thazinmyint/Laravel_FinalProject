4495bc88e6c5c3163bc84856b5e1b3f6f94c40ef
ThaZinMyint <tm129105@gmail.com>
2021-09-22 15:40:08 +0630

    make post delete and update button make dynamic with role permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -27,6 +28,15 @@ class AuthServiceProvider extends ServiceProvider
         Gate::define('admin',function(User $user){
             // $user=auth()->user
             return $user->isAdmin=='1';
+        });
+        Gate::define('premiumAdminPostowner',function(User $user,$id){
+            // post owner //post ->user_id->current_user_id==user_id
+            $post_data=Post::find($id);
+            $postOwnerId=$post_data->user_id;
+            // admin
+            // premium
+            // or gate
+            return $user->isAdmin=='1' || $user->isPremium=='1' || $user->id==$postOwnerId;
         });
     }
 }
--- resources/views/user/ShowPost.blade.php
+++ resources/views/user/ShowPost.blade.php
@@ -4,8 +4,10 @@
     <img src="{{asset('images/posts/'.$post->image)}}"  width="1000px" height="600px" class="mr-3">
     <p class="mt-3">{{$post->content}}</p>
     <div class="text-center">
+    @can("premiumAdminPostowner",$post->id)
         <a href="{{route('editPost',$post->id)}}" class="btn btn-success">Edit Post</a>
         <a href="{{route('deletePost',$post->id)}}" class="btn btn-danger ml-3">Delete Post</a>
+    @endcan
     </div>
 
 </div>
