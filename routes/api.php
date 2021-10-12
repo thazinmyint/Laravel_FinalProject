42a954f8da15c61c968f80bc8771ca52e39060af
ThaZinMyint <tm129105@gmail.com>
2021-10-12 15:51:13 +0630

    added api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get("/posts",function(){
+    $posts=Post::all();
+    return response()->json($posts);
+}); //www.localhost:8000.com/api/posts
+
+Route::post("/posts/create",function(){
+    $post=new Post();
+    $post->user_id=request('user_id');
+    $post->title=request('title');
+    $post->content=request('content');
+    $post->image=request('image');
+    $post->save();
+
+    return "post created"; //
+
 });
